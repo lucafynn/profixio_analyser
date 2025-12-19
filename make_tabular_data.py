de2de0dace3ff63a5a37edbefe5b3ae9e7eb00f9
lufynnca <lf.eckelmann@gmail.com>
2025-12-19 19:56:18 +0100

    Added correct formatting

--- make_tabular_data.py
+++ make_tabular_data.py
@@ -6,34 +6,38 @@ import numpy as np
 from pathlib import Path
 from rapidfuzz import fuzz
 
-PRINTED_WARNINGS = set()    
+PRINTED_WARNINGS = set()
+
 
 def assert_correct_table_structure(df: pd.DataFrame):
-    #TODO: implement
+    # TODO: implement
     pass
+
 
 def save_df_to_csv(df: pd.DataFrame, output_path: str):
     df.to_csv(output_path, index=False)
-    
-def match_team(lag, home, away, threshold = 50):
+
+
+def match_team(lag, home, away, threshold=50):
     if pd.isna(lag):
         return None, 0
-    
+
     scores = {
         home: fuzz.token_sort_ratio(lag, home),
-        away: fuzz.token_sort_ratio(lag, away)
+        away: fuzz.token_sort_ratio(lag, away),
     }
-    
+
     best_team = max(scores, key=scores.get)
     best_score = scores[best_team]
-    
+
     if best_score < threshold:
         msg = f"No close match between {lag} and {best_team} with score {round(best_score,2)}"
         if msg not in PRINTED_WARNINGS:
             print(msg)
             PRINTED_WARNINGS.add(msg)
-    
+
     return best_team
+
 
 def clean_shf_a_protocol(df: pd.DataFrame) -> pd.DataFrame:
     """
@@ -45,23 +49,23 @@ def clean_shf_a_protocol(df: pd.DataFrame) -> pd.DataFrame:
     df = df[df["Händelse"] != "Spelare aktiverad"]
     # Replace all cells with empty strings with NaN
     df.replace("", np.nan, inplace=True)
-    
 
     return df
+
 
 def extract_information(pdf_path: str) -> pd.DataFrame:
     """
     Alternative method to extract tables using tabula-py.
     """
     table_df = pd.DataFrame()
-    exprected_line_nr=1
-    
+    exprected_line_nr = 1
+
     match_id = re.findall(r"(\d*)_shf_a", Path(pdf_path).stem)[0]
 
     # Get the home and away team
     shf_m_path = pdf_path.replace("shf_a", "shf_m")
-    home_team=""
-    away_team=""
+    home_team = ""
+    away_team = ""
     with pdfplumber.open(shf_m_path) as pdf:
         for page in pdf.pages:
             text = page.extract_text()
@@ -79,54 +83,57 @@ def extract_information(pdf_path: str) -> pd.DataFrame:
             text = page.extract_text()
 
             # iterate over all lines in text
-            for line in text.split('\n'):
+            for line in text.split("\n"):
                 if line[0].isdigit():
                     line_nr = re.findall(r"^\d{1,3}", line)[0]
-                    #print(f"line_nr: {line_nr}, exprected_line_nr: {exprected_line_nr}")
+                    # print(f"line_nr: {line_nr}, exprected_line_nr: {exprected_line_nr}")
                     assert line_nr is not None
                     assert int(line_nr) == int(exprected_line_nr)
                     exprected_line_nr += 1
-                    
+
                     if "Spelare aktiverad" in line:
-                        match = re.findall(r"\d{1,3} \d{1,2}:\d{1,2} (.*) Spelare aktiverad \d{1,2} (.*)", line)[0]
+                        match = re.findall(
+                            r"\d{1,3} \d{1,2}:\d{1,2} (.*) Spelare aktiverad \d{1,2} (.*)",
+                            line,
+                        )[0]
                         team = match_team(match[0].strip(), home_team, away_team)
                         if team not in starters.keys():
                             starters[team] = []
-                        
+
                         starters[team].append(match[1].strip())
-                        assert len(starters.keys()) <= 2                 
-                        
+                        assert len(starters.keys()) <= 2
                     else:
                         try:
-                            full_pattern = r"\d{1,3} (\d{1,2}:\d{1,2}) (\d{1,2}-\d{1,2})?(.*)(Mål|Utvisning|Tilldömd|7-m miss|Direkt rött kort|Mål 7-m) (\d{1,2}) (.*)"
+                            full_pattern = r"\d{1,3} (\d{1,2}:\d{1,2}) (\d{1,2}-\d{1,2})?(.*)(Mål|Utvisning|Tilldömd|7-m miss|Direkt rött kort|Mål 7-m| ) (\d{1,2}) (.*)"
                             matches = re.findall(full_pattern, line)
-                            
 
-                            if matches and len(matches)==1:
+                            if matches and len(matches) == 1:
                                 match = matches[0]
                                 new_row = {
-                                    'Tid': match[0], 
-                                    'Mål': match[1].strip() if match[2] else "", 
-                                    'Lag': match[2].strip(), 
-                                    'Händelse': match[3].strip(), 
-                                    'Nr': match[4].strip(), 
-                                    'Spelare': match[5].strip()
-                                    }
-                                table_df = pd.concat([table_df, pd.DataFrame([new_row])], ignore_index=True)
+                                    "Tid": match[0],
+                                    "Mål": match[1].strip() if match[2] else "",
+                                    "Lag": match[2].strip(),
+                                    "Händelse": match[3].strip(),
+                                    "Nr": match[4].strip(),
+                                    "Spelare": match[5].strip(),
+                                }
+                                table_df = pd.concat(
+                                    [table_df, pd.DataFrame([new_row])],
+                                    ignore_index=True,
+                                )
                         except Exception as e:
-                            print(f"Error processing line in file {pdf_path}: {line}. Error: {e}")
+                            print(
+                                f"Error processing line in file {pdf_path}: {line}. Error: {e}"
+                            )
                             continue
-        
+
         clean = clean_shf_a_protocol(table_df)
-        
+
         # Match Lag (may be different in shf_a) to correct value
         clean["Lag"] = clean.apply(
-            lambda row: pd.Series(
-                match_team(row["Lag"], home_team, away_team)
-            ),
-            axis=1
+            lambda row: pd.Series(match_team(row["Lag"], home_team, away_team)), axis=1
         )
-        
+
         clean.insert(0, "match_id", match_id)
 
         meta_info = {
@@ -134,18 +141,25 @@ def extract_information(pdf_path: str) -> pd.DataFrame:
             "home_team": home_team,
             "away_team": away_team,
             "starter_home_team": str(starters[home_team]),
-            "starter_away_team": str(starters[away_team])
+            "starter_away_team": str(starters[away_team]),
         }
         return clean, meta_info
-                    
+
+
 if __name__ == "__main__":
-    protocol_files = [os.path.join("./data/protocols/shf_a", f) for f in os.listdir("./data/protocols/shf_a") if f.endswith(".pdf")]
+    protocol_files = [
+        os.path.join("./data/protocols/shf_a", f)
+        for f in os.listdir("./data/protocols/shf_a")
+        if f.endswith(".pdf")
+    ]
     full_meta_info = []
     for protocol_file in protocol_files:
         df, meta_info = extract_information(protocol_file)
         full_meta_info.append(meta_info)
-        
-        output_csv_path = Path(protocol_file.replace(".pdf", ".csv").replace("protocols", "tabular_data"))
+
+        output_csv_path = Path(
+            protocol_file.replace(".pdf", ".csv").replace("protocols", "tabular_data")
+        )
         output_csv_path.parent.mkdir(parents=True, exist_ok=True)
         save_df_to_csv(df, output_csv_path)
     meta_info_df = pd.DataFrame(full_meta_info, columns=full_meta_info[0].keys())
